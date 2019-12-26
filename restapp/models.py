f1c66eadb8ce70759b562aeb93ad7f2b60ef80e8
webizfabulous <webizmoxx@gmail.com>
2019-12-26 11:03:19 +0545

    Added models

--- restapp/models.py
+++ restapp/models.py
@@ -6,5 +6,24 @@ class CustomUser(AbstractUser):
     pass
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
 
 
+class Book(models.Model):
+    author = models.ManyToManyField(Author)
+    title = models.CharField(max_length=50)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+
+    
