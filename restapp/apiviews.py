6baf3d669bedd9e8fa426f9af053ad13864d69d2
webizfabulous <webizmoxx@gmail.com>
2019-12-26 11:18:03 +0545

    Added viewsets

--- restapp/apiviews.py
+++ restapp/apiviews.py
@@ -1,1 +1,22 @@
+from rest_framework import viewsets
+
+
+from .serializers import (
+    AuthorSerializer,
+    BookSerializer
+)
+from .models import (
+    Author,
+    Book
+)
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    serializer_class = AuthorSerializer
+    queryset = Author.objects.all()
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
 
