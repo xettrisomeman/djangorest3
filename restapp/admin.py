36b85114971abe00c8a84934d57c31fc054c736b
webizfabulous <webizmoxx@gmail.com>
2019-12-26 11:16:09 +0545

    Added serializers

--- restapp/admin.py
+++ restapp/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import (
     CustomUser,
+    Book,
+    Author
 )
 
+admin.site.register(Author)
+admin.site.register(Book)
 admin.site.register(CustomUser)
